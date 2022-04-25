6f37249f651dd5b1536308b36df5ab58a9a5f041
Tyali Warren <tyaliwarren@MacBook-Pro-3.lan>
2022-04-25 16:37:47 -0400

    MEDIUM

--- MEDIUM.js
+++ MEDIUM.js
@@ -2,8 +2,56 @@
 //and its corresponding month. For example: if the user enters the number 3, then it should return the
 //month “March.” Alert the user if they enter an invalid number (e.g. less than 1 or greater than 12).
 
-let userInput = prompt("Enter a number from 1-12");
+let userInput = Number (prompt("Enter a number from 1-12"));
 
-if (userInput == 1) {
+if (userInput === 1) {
     alert("January")
+}
+else if (userInput === 2) {
+    alert("February")
+}
+
+else if (userInput === 3) {
+    alert("March")
+}
+
+else if (userInput === 4) {
+    alert("April")
+}
+
+else if (userInput === 5) {
+    alert("May")
+}
+
+else if (userInput === 6) {
+    alert("June")
+}
+
+else if (userInput === 7) {
+    alert("July")
+}
+
+else if (userInput === 8) {
+    alert("August")
+}
+
+else if (userInput === 9) {
+    alert("September")
+}
+
+else if (userInput === 10) {
+    alert("October")
+}
+
+else if (userInput === 11) {
+    alert("November")
+}
+
+else if (userInput === 12) {
+    alert("December")
+}
+
+else {
+    alert("Invaild number, please try again")
 };
+
